c6cbdfa696cb960dceab7cc7858b1f50ed881412
LaraPluys <61267694+LaraPluys@users.noreply.github.com>
2021-05-20 20:35:22 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using ITtopicsProject.Models;
+using ITtopicsProject.ViewModels;
+using ITtopicsProject.Views;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITtopicsProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindowViewModel mainviewModel = new MainWindowViewModel();
+            MainWindow mainwindow = new MainWindow();
+            mainwindow.DataContext = mainviewModel;
+            mainwindow.Show();
+        }
+    }
+}
